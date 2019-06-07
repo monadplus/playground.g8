0aedc5b54c55ae3c6f9676073d72d6c736ff651d
Arnau Abella <arnauabella@gmail.com>
2019-06-07 09:39:57 +0200

    Main test: class instead of object

--- src/main/g8/core/src/main/scala/$package$/Main.scala
+++ src/main/g8/core/src/main/scala/$package$/Main.scala
@@ -3,7 +3,7 @@ package $package$
 import cats.implicits._
 import cats.effect._
 
-object Main extends IOApp {
+class Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
     IO(println("I am a new project!")).as(ExitCode.Success)
